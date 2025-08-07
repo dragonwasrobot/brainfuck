a6e21a8679213ccaf6fe35ecbae3fd4629a13ac9
Peter Urbak <peter@dragonwasrobot.com>
2025-08-08 01:05:57 +0200

    Adds elm-test along with an initial Lexer test

---
+++ tests/LexerTests.elm
@@ -0,0 +1,26 @@
+module LexerTests exposing (..)
+
+import Expect exposing (Expectation)
+import Lexer exposing (Position, Symbol(..), Token)
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    describe "Lexer"
+        [ test "Can tokenize a simple program" <|
+            \_ ->
+                let
+                    inputTokens =
+                        "[+++]"
+
+                    expectedTokens =
+                        [ Token StartBlock (Position 1 0)
+                        , Token IncrementByte (Position 1 1)
+                        , Token IncrementByte (Position 1 2)
+                        , Token IncrementByte (Position 1 3)
+                        , Token EndBlock (Position 1 4)
+                        ]
+                in
+                Expect.equal expectedTokens (Lexer.tokenize inputTokens)
+        ]
